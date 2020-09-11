ab5cb57045300aec940f001c59f0b15ba2d4211c
Aleksey <AfanasevAV1@yandex.ru>
2020-09-11 15:26:38 +0400

    Yatube1

--- posts/models.py
+++ posts/models.py
@@ -20,5 +20,7 @@ class Post(models.Model):
     group = models.ForeignKey(Group, related_name='posts',
                               blank=True, null=True,
                               on_delete=models.CASCADE)
-
+    def __str__(self):#
+        # выводим текст поста 
+        return self.text#
 
