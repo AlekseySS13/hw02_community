69693bb2faccd1e13d98e210951215e1363b6810
Aleksey <AfanasevAV1@yandex.ru>
2020-09-12 08:32:31 +0400

    Yatube2

--- posts/admin.py
+++ posts/admin.py
@@ -4,20 +4,15 @@ from .models import Post, Group
 
 
 class GroupAdmin(admin.ModelAdmin):
-    # перечисляем поля, которые должны отображаться в админке
     list_display = ("pk","title", "slug", "description")
 
 
 class PostAdmin(admin.ModelAdmin):
-    # перечисляем поля, которые должны отображаться в админке
     list_display = ("pk","text", "pub_date", "author") 
-    # добавляем интерфейс для поиска по тексту постов
     search_fields = ("text",) 
-    # добавляем возможность фильтрации по дате
     list_filter = ("pub_date",) 
     empty_value_display = "-пусто-"
 
 
-# при регистрации модели Post источником конфигурации для неё назначаем класс PostAdmin
 admin.site.register(Post, PostAdmin)
 admin.site.register(Group, GroupAdmin) 
--- posts/models.py
+++ posts/models.py
@@ -14,13 +14,17 @@ class Group(models.Model):
 
 
 class Post(models.Model):
+    class Meta:
+        ordering = ['-pub_date']
     text = models.TextField()
     pub_date = models.DateTimeField("date published", auto_now_add=True)
     author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="posts")
     group = models.ForeignKey(Group, related_name='posts',
                               blank=True, null=True,
-                              on_delete=models.CASCADE)
-    def __str__(self):#
-        # выводим текст поста 
-        return self.text#
+                              on_delete=models.SET_NULL)
+    def __str__(self):
+        return self.text
+
+    # class Meta:
+    #     ordering = ['-pub_date']
 
